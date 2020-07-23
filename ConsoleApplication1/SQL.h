1e0baf1f4914ae9afc3e9614bce276705fd91420
Anastasia Lozytska <trall088@gmail.com>
2020-07-23 13:45:38 +0300

    added SQL.h

---
+++ ConsoleApplication1/SQL.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <list>
+#include "sqlite3.h"
+#include <string>
+class SQL {
+private:
+	
+	sqlite3* db;
+	typedef std::list<userData> UserDataList;
+
+public:
+
+	SQL() : db(nullptr) {
+
+	}
+
+	void Open(const char* dir);
+	void createTable();
+	int createIndex();
+	void insertData(UserDataList& dataList);
+	int bindName(UserDataList& dataList);
+	int selectData(UserDataList& dataList);
+	~SQL()
+	{
+		if (db != nullptr)
+			sqlite3_close(db);
+	}
+};
